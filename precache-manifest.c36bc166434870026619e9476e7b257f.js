ebc1d48f62b773690ff524ceb03ff5664a8edd87
ITStan <s.stanislavskaya@gmail.com>
2019-11-21 13:12:42 +0200

    Updates

---
+++ precache-manifest.c36bc166434870026619e9476e7b257f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afd9dfab4ac5ff978916302a8dd7f178",
+    "url": "/react-training/index.html"
+  },
+  {
+    "revision": "7cd9b568898963b36e0b",
+    "url": "/react-training/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "9636257116244bb9f598",
+    "url": "/react-training/static/js/2.03669f55.chunk.js"
+  },
+  {
+    "revision": "7cd9b568898963b36e0b",
+    "url": "/react-training/static/js/main.fe0498c7.chunk.js"
+  },
+  {
+    "revision": "38fa1e22a7081afcbae4",
+    "url": "/react-training/static/js/runtime-main.37ff590a.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-training/static/media/logo.25bf045c.svg"
+  }
+]);
